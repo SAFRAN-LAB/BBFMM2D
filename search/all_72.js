05b42ab6cc16fa2aac6f0d6f90e8d1c10546b921
Ruoxi Wang <ruoxi@stanford.edu>
2013-08-07 14:52:33 -0700

    add binary part to documentation

--- search/all_72.js
+++ search/all_72.js
@@ -5,5 +5,7 @@ var searchData=
   ['radius',['radius',['../class_h2__2_d___node.html#a413ec45a650a349124f135b1303e9248',1,'H2_2D_Node::radius()'],['../class_h2__2_d___tree.html#a7c40135ade4b0d0457e56c2be800a61e',1,'H2_2D_Tree::radius()']]],
   ['rank',['rank',['../class_h2__2_d___tree.html#a5e55504e0ec1cc4f93042a5e3c571924',1,'H2_2D_Tree']]],
   ['read_5flocation_5fh_2ehpp',['read_Location_H.hpp',['../read___location___h_8hpp.html',1,'']]],
+  ['read_5flocation_5fhtranpose_5fbinary_2ehpp',['read_Location_Htranpose_binary.hpp',['../read___location___htranpose__binary_8hpp.html',1,'']]],
+  ['read_5fmetadata_5fbbfmm2d_2ehpp',['read_metadata_BBFMM2D.hpp',['../read__metadata___b_b_f_m_m2_d_8hpp.html',1,'']]],
   ['root',['root',['../class_h2__2_d___tree.html#a85553ca20f2f3d44ce739b0d5b2ec5b3',1,'H2_2D_Tree']]]
 ];
--- search/files_72.js
+++ search/files_72.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['read_5flocation_5fh_2ehpp',['read_Location_H.hpp',['../read___location___h_8hpp.html',1,'']]]
+  ['read_5flocation_5fh_2ehpp',['read_Location_H.hpp',['../read___location___h_8hpp.html',1,'']]],
+  ['read_5flocation_5fhtranpose_5fbinary_2ehpp',['read_Location_Htranpose_binary.hpp',['../read___location___htranpose__binary_8hpp.html',1,'']]],
+  ['read_5fmetadata_5fbbfmm2d_2ehpp',['read_metadata_BBFMM2D.hpp',['../read__metadata___b_b_f_m_m2_d_8hpp.html',1,'']]]
 ];
