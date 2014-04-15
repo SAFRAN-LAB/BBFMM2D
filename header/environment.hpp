313deb7b33eb67716e033a873b210c60c8201f19
Ruoxi Wang <ruoxi@stanford.edu>
2014-04-14 23:06:47 -0700

    delete header files

--- header/environment.hpp
+++ header/environment.hpp
@@ -1,29 +1,20 @@
-/*!
- *  \copyright This Source Code Form is subject to the terms of the Mozilla Public
- *  License, v. 2.0. If a copy of the MPL was not distributed with this
- *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *  \author Sivaram Ambikasaran, Ruoxi Wang
- *  \version 3.1
- */
 /*! \file environment.hpp
  all the headers being used in BBFMM2D library
 */
 #ifndef __environment_hpp__
 #define __environment_hpp__
 
-#include"iostream"
+
+#include <iostream>
 #include <sstream>
 #include<fstream>
 #include <stdexcept>
 #include <algorithm>
 #include <cctype>
 #include <string>
-#include <stdlib.h>
 #include<vector>
 #include"cmath"
 #include"ctime"
-#include"Eigen/Dense"
-
 
 
 #endif //(__environment_hpp__)
